77d9b47fe25ee9f35aef50d9e6e7fc44691f1751
maxim3andr3 <maxim3andr3@gmail.com>
2020-01-13 11:42:05 +0100

    Updates

---
+++ precache-manifest.1ffd7d3b1322e31149a1a6e5290ba43e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7469bba322bcec24e2f8ac549b102e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7277bc58815c2534544a",
+    "url": "/monsters-rolodex/static/css/main.5c842c4d.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7277bc58815c2534544a",
+    "url": "/monsters-rolodex/static/js/main.6524ab40.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
